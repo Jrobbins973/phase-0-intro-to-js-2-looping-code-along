5f80286a93ee575a671b52a159ce7b2bf28cbc94
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-09-21 09:33:29 -0400

    commit tree b37e0f8a1b71882527feeede7ab06f6986fd6630

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const writeCards = (array) => {
+    let i = 0
+    while ( i < array.length) {
+        console.log(`Thank you ${array[i]} for the wonderful gift!`)
+        i++
+    }
+}
+
+
+
+const countDown = (num) => {
+    while (num >= 0) {
+        console.log(num--)
+    }
+}
+console.log(countDown(10))
